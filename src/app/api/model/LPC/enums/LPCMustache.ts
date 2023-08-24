3ed18115266cf2425d582fa24191ce81ac5fc41f
Zuzanna Olszewska <zolszewska@apptension.com>
2023-08-24 11:52:55 +0200

    delete mustache, add temporary optional properties

--- src/app/api/model/LPC/enums/LPCMustache.ts
+++
@@ -1,4 +0,0 @@
-export enum LPCMustache {
-	mustache = 'mustache',
-	bigstache = 'bigstache',
-}
--- src/app/api/model/character/look/CharacterBodyLook.ts
+++ src/app/api/model/character/look/CharacterBodyLook.ts
@@ -1,7 +1,12 @@
+import { LPCNose } from './../../LPC/enums/LPCNose';
+import { LPCHairColor } from './../../LPC/enums/LPCHairColor';
+import { LPCBeard } from './../../LPC/enums/LPCBeard';
 import { LPCDictionary } from '../../LPC/dictionaries/LPCDictionary';
 import { LPCBodyColor } from '../../LPC/enums/LPCBodyColor';
 import { LPCEars } from '../../LPC/enums/LPCEars';
+import { LPCEyes } from '../../LPC/enums/LPCEyes';
 import { LPCGender } from '../../LPC/enums/LPCGender';
+import { LPCHair } from '../../LPC/enums/LPCHair';
 
 export class CharacterBodyLook {
 
@@ -9,7 +14,12 @@ export class CharacterBodyLook {
 	constructor(
 		private readonly gender: LPCGender,
 		private readonly bodyColor: LPCBodyColor,
-		private readonly ears: LPCEars
+		private readonly ears: LPCEars,
+		private readonly eyes?: LPCEyes,
+		private readonly nose?: LPCNose,
+		private readonly beard?: LPCBeard,
+		private readonly hairColor?: LPCHairColor,
+		private readonly hair?: LPCHair
 	) {
 	}
 
