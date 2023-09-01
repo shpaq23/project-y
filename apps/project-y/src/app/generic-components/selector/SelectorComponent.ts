944655a3fa657ffc2a2f232034472bb0ee7c3444
Zuzanna Olszewska <zolszewska@apptension.com>
2023-09-01 12:24:44 +0200

    add disabled to button

--- apps/project-y/src/app/generic-components/selector/SelectorComponent.ts
+++ apps/project-y/src/app/generic-components/selector/SelectorComponent.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
+import { BaseComponent } from '../../utils/BaseComponent';
 import { ButtonComponent } from '../button/ButtonComponent';
+import { BrowserModule } from '@angular/platform-browser'
 
 @Component({
 	selector: 'selector',
@@ -10,9 +12,9 @@ import { ButtonComponent } from '../button/ButtonComponent';
 	styleUrls: ['./SelectorComponent.scss'],
 	standalone: true,
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	imports: [ButtonComponent, FontAwesomeModule]
+	imports: [ButtonComponent, FontAwesomeModule, BrowserModule]
 })
-export class SelectorComponent implements OnInit {
+export class SelectorComponent extends BaseComponent implements OnInit {
 
 	@Input({ required: true })
 	options!: Array<string>;
@@ -25,6 +27,13 @@ export class SelectorComponent implements OnInit {
 
 	arrowLeft = faArrowLeft;
 	arrowRight = faArrowRight;
+
+
+	constructor(
+		private readonly changeDetectorRef: ChangeDetectorRef
+	) {
+		super();
+	}
 
 	ngOnInit(): void {
 		if (!this.selectedOption) {
